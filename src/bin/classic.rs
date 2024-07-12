ba96cb082c15c80410546bff46e6d1c440e2275b
Cheezer1656 <118921295+Cheezer1656@users.noreply.github.com>
2024-07-12 14:35:22 -0700

    Start on classic

---
+++ src/bin/classic.rs
@@ -0,0 +1,190 @@
+#![allow(clippy::type_complexity)]
+
+#[path = "../lib/mod.rs"]
+mod lib;
+
+use bevy_ecs::query::WorldQuery;
+use lib::config::*;
+use lib::game::*;
+use valence::entity::living::Health;
+use valence::entity::Velocity;
+use valence::entity::{EntityId, EntityStatuses};
+use valence::math::Vec3Swizzles;
+use valence::prelude::*;
+use valence::protocol::packets::play::DamageTiltS2c;
+use valence::protocol::sound::SoundCategory;
+use valence::protocol::Sound;
+use valence::protocol::VarInt;
+use valence::protocol::WritePacket;
+
+pub fn main() {
+    match register_defaults(&mut App::new()) {
+        Ok(app) => {
+            app.add_systems(EventLoopUpdate, handle_combat_events)
+                .add_systems(
+                    Update,
+                    (
+                        start_game.after(lib::game::start_game),
+                        end_game.after(lib::game::end_game),
+                        handle_oob_clients,
+                    ),
+                )
+                .run();
+        }
+        Err(e) => eprintln!("{}", e),
+    }
+}
+
+fn start_game(
+    mut clients: Query<&mut Inventory>,
+    games: Query<&Entities>,
+    mut start_game: EventReader<StartGameEvent>,
+) {
+    for event in start_game.read() {
+        if let Ok(entities) = games.get(event.0) {
+            for entity in entities.0.iter() {
+                if let Ok(mut inv) = clients.get_mut(*entity) {
+                    inv.set_slot(36, ItemStack::new(ItemKind::IronSword, 1, None));
+                }
+            }
+        }
+    }
+}
+
+fn end_game(
+    mut clients: Query<&mut Inventory>,
+    games: Query<&Entities>,
+    mut start_game: EventReader<StartGameEvent>,
+) {
+    for event in start_game.read() {
+        if let Ok(entities) = games.get(event.0) {
+            for entity in entities.0.iter() {
+                if let Ok(mut inv) = clients.get_mut(*entity) {
+                    for slot in 0..inv.slot_count() {
+                        inv.set_slot(slot, ItemStack::EMPTY);
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[derive(WorldQuery)]
+#[world_query(mutable)]
+struct CombatQuery {
+    client: &'static mut Client,
+    id: &'static EntityId,
+    pos: &'static Position,
+    vel: &'static mut Velocity,
+    health: &'static mut Health,
+    state: &'static mut CombatState,
+    statuses: &'static mut EntityStatuses,
+    gamestate: &'static PlayerGameState,
+}
+
+fn handle_combat_events(
+    server: Res<Server>,
+    mut clients: Query<CombatQuery>,
+    mut sprinting: EventReader<SprintEvent>,
+    mut interact_entity: EventReader<InteractEntityEvent>,
+    mut end_game: EventWriter<EndGameEvent>,
+) {
+    for &SprintEvent { client, state } in sprinting.read() {
+        if let Ok(mut client) = clients.get_mut(client) {
+            client.state.has_bonus_knockback = state == SprintState::Start;
+        }
+    }
+
+    for &InteractEntityEvent {
+        client: attacker_client,
+        entity: victim_client,
+        interact: interaction,
+        ..
+    } in interact_entity.read()
+    {
+        let Ok([mut attacker, mut victim]) = clients.get_many_mut([attacker_client, victim_client])
+        else {
+            continue;
+        };
+
+        if interaction != EntityInteraction::Attack
+            || server.current_tick() - victim.state.last_attacked_tick < 10
+            || attacker.gamestate.game_id != victim.gamestate.game_id
+        {
+            continue;
+        }
+
+        victim.state.last_attacked_tick = server.current_tick();
+
+        let victim_pos = victim.pos.0.xz();
+        let attacker_pos = attacker.pos.0.xz();
+
+        let dir = (victim_pos - attacker_pos).normalize().as_vec2();
+
+        let knockback_xz = if attacker.state.has_bonus_knockback {
+            18.0
+        } else {
+            8.0
+        };
+        let knockback_y = if attacker.state.has_bonus_knockback {
+            8.432
+        } else {
+            6.432
+        };
+
+        victim
+            .client
+            .set_velocity([dir.x * knockback_xz, knockback_y, dir.y * knockback_xz]);
+
+        let damage = 5.83;
+        if victim.health.0 > damage {
+            victim.health.0 -= damage;
+        } else {
+            end_game.send(EndGameEvent {
+                game_id: victim.gamestate.game_id.unwrap(),
+                loser: victim.gamestate.team,
+            });
+        }
+
+        attacker.state.has_bonus_knockback = false;
+
+        victim.client.play_sound(
+            Sound::EntityPlayerHurt,
+            SoundCategory::Player,
+            victim.pos.0,
+            1.0,
+            1.0,
+        );
+        victim.client.write_packet(&DamageTiltS2c {
+            entity_id: VarInt(0),
+            yaw: 0.0,
+        });
+        attacker.client.play_sound(
+            Sound::EntityPlayerHurt,
+            SoundCategory::Player,
+            victim.pos.0,
+            1.0,
+            1.0,
+        );
+        attacker.client.write_packet(&DamageTiltS2c {
+            entity_id: VarInt(victim.id.get()),
+            yaw: 0.0,
+        });
+    }
+}
+
+fn handle_oob_clients(
+    positions: Query<(&Position, &PlayerGameState), With<Client>>,
+    mut end_game: EventWriter<EndGameEvent>,
+) {
+    for (pos, gamestate) in positions.iter() {
+        if pos.0.y < 0.0 {
+            if gamestate.game_id.is_some() {
+                end_game.send(EndGameEvent {
+                    game_id: gamestate.game_id.unwrap(),
+                    loser: gamestate.team,
+                });
+            }
+        }
+    }
+}
