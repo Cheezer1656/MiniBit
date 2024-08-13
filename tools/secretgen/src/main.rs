4405c86e7e5a18229e512b83bec0a4675de8007b
Cheezer1656 <118921295+Cheezer1656@users.noreply.github.com>
2024-08-13 15:04:46 -0700

    Create secretgen tool

---
+++ tools/secretgen/src/main.rs
@@ -0,0 +1,10 @@
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+
+fn main() {
+    let mut rng = thread_rng();
+    let s: String = (0..12)
+        .map(|_| rng.sample(Alphanumeric) as char)
+        .collect();
+    println!("{}", s);
+}
