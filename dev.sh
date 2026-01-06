c9adebf5cc405e052cbe0ec3b4865fc923c2a96b
pythonsnake602 <118955679+pythonsnake602@users.noreply.github.com>
2026-01-05 18:54:02 -0800

    Fix dev.sh, remove unused gate config, and add gate motd

--- dev.sh
+++ dev.sh
@@ -4,5 +4,5 @@
 
 (cd gate && go build -ldflags "-s -w")
 
-set -a && source run.tmp/.env && set+a
+set -a && source run.tmp/.env && set +a
 tmux new-session -d "cargo run -- --auto run.tmp" \; split-window "GATE_VELOCITY_SECRET=$FORWARDING_SECRET gate/gate -c run.tmp/proxy/config.yml" \; attach
