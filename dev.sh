14cf152660691b579a15c18b3cedf4a678af5451
pythonsnake602 <118955679+pythonsnake602@users.noreply.github.com>
2026-01-06 17:29:04 -0800

    Update dev.sh for velocity

--- dev.sh
+++ dev.sh
@@ -2,7 +2,16 @@
 
 ./configure_servers.sh
 
-(cd gate && go build -ldflags "-s -w")
+function build_velocity {
+  cd velocity
+  gradle build
+  mkdir -p ../run.tmp/proxy/plugins
+  cp build/libs/*.jar ../run.tmp/proxy/plugins
+
+  curl --output ../run.tmp/proxy/velocity.jar "https://api.papermc.io/v2/projects/velocity/versions/3.3.0-SNAPSHOT/builds/415/downloads/velocity-3.3.0-SNAPSHOT-415.jar"
+}
+
+(build_velocity)
 
 set -a && source run.tmp/.env && set +a
-tmux new-session -d "cargo run -- --auto run.tmp" \; split-window "GATE_VELOCITY_SECRET=$FORWARDING_SECRET gate/gate -c run.tmp/proxy/config.yml" \; attach
+tmux new-session -d "cargo run -- --auto run.tmp" \; split-window "VELOCITY_FORWARDING_SECRET=$FORWARDING_SECRET cd run.tmp/proxy && java -jar ./velocity.jar" \; attach
