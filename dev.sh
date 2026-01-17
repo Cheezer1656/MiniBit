e303f8e3a3c38a5bb2137ab7f2bf390acd789a34
pythonsnake602 <118955679+pythonsnake602@users.noreply.github.com>
2026-01-17 12:48:59 -0800

    Add viaversion plugins to velocity (#18)
    * Add viaversion plugins to velocity
    * Update velocity version in dev.sh to 3.3.0-SNAPSHOT-436

--- dev.sh
+++ dev.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-mkdir -p run.tmp/proxy
+mkdir -p run.tmp/proxy/plugins
 
 velocity_jar="run.tmp/proxy/velocity.jar"
 if [ ! -e "$velocity_jar" ]; then
-  curl --output "$velocity_jar" "https://api.papermc.io/v2/projects/velocity/versions/3.3.0-SNAPSHOT/builds/415/downloads/velocity-3.3.0-SNAPSHOT-415.jar"
+  curl --output "$velocity_jar" "https://api.papermc.io/v2/projects/velocity/versions/3.3.0-SNAPSHOT/builds/436/downloads/velocity-3.3.0-SNAPSHOT-436.jar"
+fi
+
+viaversion_jar="run.tmp/proxy/plugins/viaversion.jar"
+if [ ! -e "$viaversion_jar" ]; then
+  curl --output "$viaversion_jar" "https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.7.1-SNAPSHOT%2B897/PAPER/ViaVersion-5.7.1-SNAPSHOT.jar"
+fi
+viabackwards_jar="run.tmp/proxy/plugins/viabackwards.jar"
+if [ ! -e "$viabackwards_jar" ]; then
+  curl --output "$viabackwards_jar" "https://hangarcdn.papermc.io/plugins/ViaVersion/ViaBackwards/versions/5.7.1-SNAPSHOT%2B544/PAPER/ViaBackwards-5.7.1-SNAPSHOT.jar"
+fi
+viarewind_jar="run.tmp/proxy/plugins/viarewind.jar"
+if [ ! -e "$viarewind_jar" ]; then
+  curl --output "$viarewind_jar" "https://hangarcdn.papermc.io/plugins/ViaVersion/ViaRewind/versions/4.0.13/PAPER/ViaRewind-4.0.13.jar"
 fi
 
 function build_velocity {
@@ -13,7 +26,6 @@ function build_velocity {
   mkdir -p ../run.tmp/proxy/plugins
   cp build/libs/*.jar ../run.tmp/proxy/plugins
 }
-
 
 (build_velocity)
 
